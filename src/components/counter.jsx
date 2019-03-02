8c499188f72b60005c1d3742d23bc73b211e5ab1
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-02 21:02:45 +0000

    created counter componet and imported bootstrap

---
+++ src/components/counter.jsx
@@ -0,0 +1,19 @@
+// .jsx is jjust for better code completion
+
+// video 24
+// counter module
+// import react and component class from react module
+
+// note there are shortcuts to the below code with the extension 'simple react snippets'
+
+// type imrc and tab for the below
+import React, { Component } from 'react';
+
+// type cc for this shortcut
+class  Counter extends Component {
+    render() { 
+        return <h1>Hello World im a component</h1>;
+    }
+}
+ 
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//import component
+import Counter from './components/counter';
+
+// import bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
+
+ReactDOM.render(<Counter />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
