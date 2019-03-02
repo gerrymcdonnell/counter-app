d062eeaf7a0b31130ed20623bf2eb709a3cb5243
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-02 21:25:24 +0000

    added imgeurl attritbute to counter component

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -13,14 +13,16 @@ import React, { Component } from "react";
 class Counter extends Component {
   //state object that contaisn data the component needs
   state = {
-    count: 0
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
   };
   render() {
     return (
-      <React.Fragment>
+      <div>
+        <img src={this.state.imageUrl} alt="" />
         <span>{this.formatCount()}</span>
         <button>Increment</button>
-      </React.Fragment>
+      </div>
     );
   }
 
