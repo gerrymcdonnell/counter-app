381a80a3311864c693d77b0369ed78db62cdd0cb
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-07 10:43:08 +0000

    added boostrap classname to span and button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,12 +16,19 @@ class Counter extends Component {
     count: 0,
     imageUrl: "https://picsum.photos/200"
   };
+
+  styles={
+    fontSize:10,
+    fontWeight: "bold"
+  };
+
   render() {
     return (
       <div>
         <img src={this.state.imageUrl} alt="" />
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button>Increment</button>
+        
+        <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
