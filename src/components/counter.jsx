fb6137736ecaa2572961e83489bf784dc1000f84
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-07 10:57:58 +0000

    added conditional rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -24,11 +24,12 @@ class Counter extends Component {
   };
 
   render() {
+
     return (
       <div>
         <img src={this.state.imageUrl} alt="" />
 
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
@@ -41,9 +42,16 @@ class Counter extends Component {
     );
   }
 
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     return this.state.count === 0 ? "Zero" : this.state.count;
   }
+
 }
 
 export default Counter;
