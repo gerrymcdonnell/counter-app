2989bac3b70c2b19229dcf9d18f5ea2c6c7d7ace
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-14 12:47:58 +0000

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
-import Navbar from './components/navbar';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/navbar";
+import "./App.css";
+//import component
+import Counters from "./components/counters";
 
 class App extends Component {
   render() {
     return (
-      <Navbar>
-        <main className ="container">
-        
+      /* if returning multiple root elements from root method must wrap in a react fragment*/
+      <React.Fragment>
+        <Navbar />
+        <main className="container">
+          <Counters />
         </main>
-      </Navbar>
+      </React.Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
-//import component
-import Counters from "./components/counters";
-
 // import bootstrap v4
 import "bootstrap/dist/css/bootstrap.css";
 
