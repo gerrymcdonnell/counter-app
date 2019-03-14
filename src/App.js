60526260921ac77ecfea688c4410cc79d3098fbc
Gerry Mc Donnell <irishbloke@gmail.com>
2019-03-14 13:09:56 +0000

    added 2 counter methods to show number of counters

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ import Counters from "./components/counters";
 class App extends Component {
   state = {
     counters: [
-      { id: 1, value: 4 },
+      { id: 1, value: 1 },
       { id: 2, value: 2 },
       { id: 3, value: 3 },
-      { id: 4, value: 4 }
+      { id: 4, value: 0 }
     ]
   };
 
@@ -49,7 +49,17 @@ class App extends Component {
     return (
       /* if returning multiple root elements from root method must wrap in a react fragment*/
       <React.Fragment>
+        
+        {/**total counters */}
         <Navbar totalCounters={this.state.counters.length} />
+
+        {/**total counters with a value greater than 0 
+        filter the counters array to contain only the counters with a value>0 and then count 
+        the elements in the array
+        */}
+        <Navbar
+          totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
+
         <main className="container">
           <Counters
             counters={this.state.counters}
